a7f8b3f44977e58be2b90e4a0769fb1102f0100b
wulei15157 <353540447@qq.com>
2021-02-23 13:28:31 +0800

    ''

--- Grunt/Gruntfile.js
+++ Grunt/Gruntfile.js
@@ -38,16 +38,39 @@ jshint : {
   build : ['Gruntfile.js', 'src/js/*.js'] //指定检查的文件
 },
 
-
+//使用css-min插件 css 合并压缩
+cssmin:{
+  options: {
+    shorthandCompacting: false,
+    roundingPrecision: -1
+  },
+  build: {
+    files: {
+        'build/css/output.min.css': ['src/css/*.css']
+    }
+  }
+},
+watch : {
+  scripts : {
+    files : ['src/js/*.js', 'src/css/*.css'],
+    tasks : ['concat', 'jshint', 'uglify', 'cssmin'],
+    options : {spawn : false}  
+  }
+}
+ 
 
 
   })
   // 2. 加载插件任务
-	grunt.loadNpmTasks('grunt-contrib-concat')
-	grunt.loadNpmTasks('grunt-contrib-uglify')
-	grunt.loadNpmTasks('grunt-contrib-jshint')
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 
-  // 3. 注册构建任务 grunt 执行任务是同步的
-	grunt.registerTask('default', ['concat','uglify','jshint'])
+  // 3. 注册构建任务 grunt 执行任务是同步的	
+	grunt.registerTask('default', ['concat','uglify','jshint','cssmin']);
+   // 
+	grunt.registerTask('myWatch', ['default','watch']);
 }
